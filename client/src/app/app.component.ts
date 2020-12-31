6d24f36985b9ac380eddbee1b59dee0c36208bec
Nouman Baloch <nomibaloch775@hotmail.com>
2020-12-31 23:38:46 +0500

    Initial Setup for Angular Client App

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Skinet';
+  products: IProduct[];
+
+  constructor(private http:HttpClient){}
+  
+  ngOnInit(): void {
+    this.http.get('https://localhost:44376/api/products?pageSize=50').subscribe(
+      (response: IPagination) => {
+        this.products = response.data;
+      }, error => {
+        console.log(error);
+      });
+  }
 }
